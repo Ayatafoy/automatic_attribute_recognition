bd3ff08510761165c89263747a939c2c5938d386
Ayatafoy <romanow.lesha2013@yandex.ru>
2021-04-16 21:14:23 +0400

    add dress length model

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def init_sl_model():
     model = load_model("models/sleeve_length_classifier.hdf5")
     return model
 
+@st.cache(allow_output_mutation=True)
+def init_dl_model():
+    model = load_model("models/dress_length_classifier.hdf5")
+    return model
+
 def predict_sleeve_length(img_pixels_tensor):
     with tf.device('/cpu'):
         model = init_sl_model()
@@ -19,8 +24,16 @@ def predict_sleeve_length(img_pixels_tensor):
 
     return preds
 
+def predict_dress_length(img_pixels_tensor):
+    with tf.device('/cpu'):
+        model = init_dl_model()
+        preds = model.predict(img_pixels_tensor)
+
+    return preds
+
 try:
-    classes = ['3 / 4 Sleeve', 'Short Sleeve', 'Sleeveless', 'Long Sleeve']
+    sl_classes = ['3 / 4 Sleeve', 'Short Sleeve', 'Sleeveless', 'Long Sleeve']
+    dl_classes = ['long', 'short', 'midi', 'knee']
     st.write('\n')
 
     image = Image.open('images/image.png')
@@ -52,14 +65,21 @@ try:
                     img_pixels = cv2.resize(img_pixels, (256, 256))
                     img_pixels = np.expand_dims(img_pixels, axis=0)
                     img_pixels_tensor = tf.convert_to_tensor(img_pixels, dtype=tf.int32)
-                    preds = predict_sleeve_length(img_pixels_tensor)
-                    prediction = np.argmax(preds)
+                    sl_preds = predict_sleeve_length(img_pixels_tensor)
+                    dl_preds = predict_dress_length(img_pixels_tensor)
+                    sl_prediction = np.argmax(sl_preds)
+                    dl_prediction = np.argmax(dl_preds)
                     st.success('Done!')
                     st.sidebar.header("Algorithm Predicts: ")
-                    probability = "{:.3f}".format(float(preds[0][prediction] * 100))
-                    st.sidebar.write(f'Predicted sleeve length: ', classes[prediction])
+                    sl_probability = "{:.3f}".format(float(sl_preds[0][sl_prediction] * 100))
+                    dl_probability = "{:.3f}".format(float(dl_preds[0][dl_prediction] * 100))
+                    st.sidebar.write(f'Predicted sleeve length: **{sl_classes[sl_prediction]}**')
                     st.sidebar.write('\n')
-                    st.sidebar.write('**Probability: **', preds[0][prediction] * 100, '%')
+                    st.sidebar.write('Probability: ', sl_preds[0][sl_prediction] * 100, '%')
+                    st.sidebar.write('\n')
+                    st.sidebar.write(f'Predicted dress length: **{dl_classes[dl_prediction]}**')
+                    st.sidebar.write('\n')
+                    st.sidebar.write('Probability: ', dl_preds[0][dl_prediction] * 100, '%')
                 except Exception as e:
                     st.sidebar.error(e)
                     st.sidebar.error("This file format is not supported. Please try to upload another image...")
